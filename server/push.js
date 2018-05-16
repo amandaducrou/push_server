921b9cae57c907082947ddc6f7196ad66630c1c3
Amanda Ducrou <aducrou@expedia.com>
2018-05-16 11:16:06 +1000

    End of lesson 36

--- server/push.js
+++ server/push.js
@@ -1,23 +1,28 @@
 // Modules
 const webpush = require("web-push");
 const urlsafeBase64 = require("urlsafe-base64");
+const Storage = require("node-storage");
 
 // Vapid Keys
 const vapid = require("./vapid.json");
 
 // Subscriptions
-let subscriptions = [];
+const subscriptionsKey = "subscriptions";
+const store = new Storage(`${__dirname}/db`);
+
+let subscriptions = store.get(subscriptionsKey) || [];
 
 // Create URL safe Vapid public key
-module.export.getKey = () => {
+module.exports.getKey = () => {
     return urlsafeBase64.decode(vapid.publicKey)
 };
 
 // Store new subscription
-module.export.addSubscription = (subscription) => {
+module.exports.addSubscription = (subscription) => {
 
     // Add to subscriptions array
     subscriptions.push(subscription);
 
-    console.log(subscriptions);
+    // Persist subscriptions
+    store.put(subscriptionsKey, subscriptions);
 };
